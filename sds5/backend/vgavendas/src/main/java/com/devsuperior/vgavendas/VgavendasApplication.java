a03dc58d302258a0b65cb14cd5702fe0bc2575ca
Vitusan <0vetor0@gmail.com>
2021-11-01 17:24:11 -0300

    Project created

---
+++ sds5/backend/vgavendas/src/main/java/com/devsuperior/vgavendas/VgavendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.vgavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VgavendasApplication.class, args);
+	}
+
+}
---
+++ sds5/backend/vgavendas/src/test/java/com/devsuperior/vgavendas/VgavendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.vgavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VgavendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
