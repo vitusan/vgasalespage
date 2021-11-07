b24498d8016f11b936616448bb4305825257e9db
Vitusan <0vetor0@gmail.com>
2021-11-07 12:50:28 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,22 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
-        <div className="container">
-            <h2>HELLO WORLD FROM HOME PAGE</h2>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">VGA Vendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <Link className='btn btn-primary btn-lg' to='/dashboard'>Acessar o DashBoard</Link>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
